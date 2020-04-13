33bb7ed915d01e185fb7808a3945fb3a61e2bf09
gmzglmz;glmz;gm <44279195+AhmedMagdyii@users.noreply.github.com>
2020-04-13 13:48:32 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    TOKEN: 'Njg5ODk2OTY3NDA4NzEzNzQz.XosxRw.OZomXQHOEsevxNoIEmyf7iYBUCk',  
-    Prime1: '112Njk3ODYzNjMyNDA2NDQ2MTUw.Xo9eLw.nbB-fSsfPT6rwNMq_axvWYijxW8',
+    TOKEN: '',  
+    Prime1: '',
    // TOKEN2: 'NTc2MTg5ODA5MTg2MzA4MTA2.XosTKw.t5lFW1f9F_Nk1_GHZFlGMr-893U',   
     YT_API_KEY: 'api ', /// مش مهم
     prefix: '=', // 
